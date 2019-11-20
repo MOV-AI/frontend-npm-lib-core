a18d06f2b2504b61fb2f3519d152b1bdf6f8cb48
cristovao <pedro.cristovao@mov.ai>
2019-11-20 17:38:08 +0000

    minor utils update

--- src/Utils/Utils.js
+++ src/Utils/Utils.js
@@ -4,4 +4,29 @@ const Utils = {};
 
 Utils.ofNull = x => Maybe.fromNull(x);
 
+Utils.range = (init, end) => {
+  const ans = [];
+  for (let i = 0; i < end; i++) ans.push(i);
+  return ans;
+};
+
+/**
+ *
+ * @param {*} array
+ * @param {*} groupFunction : function x => group class;
+ *
+ * Usage example:
+ *
+ * groupBy([1,2,3,4,5,6,7,8,9], x => x % 3) // {0: [3,6,9], 1:[1,4,7], 2:[2,5,8]}
+ */
+Utils.groupBy = (array, groupFunction) => {
+  const ans = {};
+  array.forEach(x => {
+    const key = groupFunction(x);
+    if (!ans[key]) ans[key] = [];
+    ans[key].push(x);
+  });
+  return ans;
+};
+
 export default Utils;
