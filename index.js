a88cf6ef4daae8cce4d2cb68d2bab4a2f7d55546
cristovao <pedro.cristovao@mov.ai>
2019-11-21 15:07:43 +0000

    change movai export testing

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 import Authentication from "./src/Authentication/Authentication";
 import Utils from "./src/Utils/Utils";
 
-const Movai = {};
-
-Movai.Authentication = Authentication;
-Movai.Utils = Utils;
-
-export default Movai;
+export { Authentication, Utils };
