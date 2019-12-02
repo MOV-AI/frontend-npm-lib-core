f9eb42861796d438f353825b27c8965aba5c04af
cristovao <pedro.cristovao@mov.ai>
2019-12-02 16:08:26 +0000

    solve webpack bug with movai export

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: ["./index.js"],
+  entry: "./index.js",
   output: {
     path: path.resolve("./"),
     filename: "dist/index.js",
