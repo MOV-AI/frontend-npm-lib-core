b6d5ab37c4abb8e94f8477cec416efe005a6c3b8
cristovao <pedro.cristovao@mov.ai>
2019-12-02 15:35:14 +0000

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ module.exports = {
   entry: ["./index.js"],
   output: {
     path: path.resolve("./"),
-    filename: "dist/index.js"
-    // library: "Movai"
+    filename: "dist/index.js",
+    library: "Movai"
   },
   module: {
     rules: [
