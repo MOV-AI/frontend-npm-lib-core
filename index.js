595b660b890deacce8faad6f098a2eb8efb825a8
cristovao <pedro.cristovao@mov.ai>
2019-12-03 15:59:12 +0000

    add MasterDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Authentication from "./src/Authentication/Authentication";
 import Utils from "./src/Utils/Utils";
 import Database from "./src/Database/Database";
+import MasterDB from "./src/Database/MasterDB";
 
-export { Authentication, Utils, Database };
+export { Authentication, Utils, Database, MasterDB };
