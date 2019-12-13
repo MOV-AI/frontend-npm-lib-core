3c05994ba954e2bca442bf4ae9bee6550abb3af0
cristovao <pedro.cristovao@mov.ai>
2019-12-13 17:26:28 +0000

    Authetication update

--- src/Authentication/Authentication.js
+++ src/Authentication/Authentication.js
@@ -1,7 +1,5 @@
 import jwtDecode from "jwt-decode";
 
-const REST_API = `http://${window.location.hostname}:${window.location.port}`;
-
 const Authentication = {};
 
 Authentication.AuthException = function(message) {
@@ -44,7 +42,7 @@ Authentication.login = async (username, password, remember) => {
   let headers = {};
   headers["Content-Type"] = "application/json";
 
-  const url = `${REST_API}/token-auth/`;
+  const url = `token-auth/`;
 
   try {
     const response = await fetch(url, {
@@ -122,7 +120,7 @@ Authentication.checkLogin = async () => {
         throw "refresh token has expired";
       }
 
-      const url = `${REST_API}/token-refresh/`;
+      const url = `token-refresh/`;
       const headers = { "Content-Type": "application/json" };
       const response = await fetch(url, {
         method: "POST",
