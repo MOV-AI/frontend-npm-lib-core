8092408802bf63b1b783d5399464f414c6ab9bc4
Vicente Queiroz <amcbd89@bitbucket.org>
2020-01-02 16:10:13 +0000

    Made sure to Exclude nodemodules from webpack bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+var nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   entry: "./index.js",
@@ -8,6 +9,8 @@ module.exports = {
     library: "Movai",
     libraryTarget: "umd"
   },
+  target: "node", // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   module: {
     rules: [
       {
