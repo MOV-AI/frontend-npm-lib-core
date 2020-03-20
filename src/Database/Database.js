0787593d35b7c690e8b9de21abdd1b0fb8d4b744
Vicente Queiroz <amcbd89@bitbucket.org>
2020-03-20 08:59:04 +0000

    Added feedback in POST

--- src/Database/Database.js
+++ src/Database/Database.js
@@ -287,9 +287,14 @@ class Database {
         }
       }).then(res => {
         if (callback) {
-          res.json().then(data => {
-            callback(data, res);
-          });
+          res
+            .json()
+            .then(data => {
+              callback(data, res);
+            })
+            .catch(e => {
+              callback(undefined, e);
+            });
         }
       });
     });
