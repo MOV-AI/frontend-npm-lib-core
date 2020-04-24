25d3e7d1e7129eec3cc5aa38e92275442ed479fb
cristovao <pedro.cristovao@mov.ai>
2020-04-24 18:52:04 +0100

    add prettier config, add jest babel config, add UndoManager

---
+++ src/UndoManager/UndoManager.js
@@ -0,0 +1,51 @@
+export default class UndoManager {
+  constructor() {
+    this.undoStack = [];
+    this.redoStack = [];
+  }
+
+  doIt(undoAbleAction) {
+    undoAbleAction.doAction();
+    this.undoStack.push(undoAbleAction);
+    this.redoStack = [];
+  }
+
+  undo() {
+    const undoAbleAction = this.undoStack.pop();
+    undoAbleAction.undoAction();
+    this.redoStack.push(undoAbleAction);
+  }
+
+  redo() {
+    const undoAbleAction = this.redoStack.pop();
+    undoAbleAction.doAction();
+    this.undoStack.push(undoAbleAction);
+  }
+
+  static actionBuilder() {
+    return new UndoAbleActionBuilder();
+  }
+}
+
+class UndoAbleActionBuilder {
+  constructor() {
+    this._doAction = null;
+    this._undoAction = null;
+  }
+
+  doAction(lambda) {
+    this._doAction = lambda;
+    return this;
+  }
+
+  undoAction(lambda) {
+    this._undoAction = lambda;
+    return this;
+  }
+
+  build() {
+    if ([this._undoAction, this._undoAction].some(x => x == null))
+      throw "Forgot to set doAction or undoAction";
+    return { doAction: this._doAction, undoAction: this._undoAction };
+  }
+}
