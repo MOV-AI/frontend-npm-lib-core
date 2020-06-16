03a6e791212ed626cd46ec7033f2d7276cf86e19
cristovao <pedro.cristovao@mov.ai>
2020-06-16 15:20:01 +0100

    add clipboard class

---
+++ src/Clipboard/Clipboard.js
@@ -0,0 +1,61 @@
+/**
+ * Clipboard singleton
+ */
+export default class Clipboard {
+  constructor() {
+    if (Clipboard.instance) return Clipboard.instance;
+    Clipboard.instance = this;
+    this._data = {};
+    this.copyKey = Math.random();
+  }
+
+  /**
+   *  get clipboard data
+   */
+  get data() {
+    return this._data;
+  }
+
+  /**
+   *
+   * @param {string} key base key to clear
+   */
+  static clear(key) {
+    const inst = new Clipboard();
+    if (key) delete inst._data[key];
+  }
+
+  /**
+   *
+   * @param {string} key base key to read
+   */
+  static read(key) {
+    const inst = new Clipboard();
+    return key ? inst.data[key] || {} : inst.data;
+  }
+
+  /**
+   *
+   * @param {string} key base key to write
+   * @param {any} value object, primitive, etc, to write
+   */
+  static write(key, value) {
+    return (new Clipboard()._data[key] = value);
+  }
+
+  /**
+   * Copies value
+   * @param {*} value
+   */
+  static copy(value) {
+    Clipboard.write(new Clipboard().copyKey, value);
+  }
+
+  /**
+   * Retrieve copied object
+   */
+  static paste() {
+    // consider making deep copy
+    return Clipboard.read(new Clipboard().copyKey);
+  }
+}
