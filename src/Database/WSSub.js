0e5d104a1c3f34ac9a730d723d9ada89242b53cd
Rian Martins <rian.martins@mov.ai>
2021-02-01 16:30:06 +0000

    Fix authentication crash if loose token key from localStorage

--- src/Database/WSSub.js
+++ src/Database/WSSub.js
@@ -138,12 +138,9 @@ class WSSub {
       // stringify the message before sending it
       // try to resend if the connection is not yet open
       this.getState() !== WebSocket.OPEN
-        ? setTimeout(
-            this.websocket.send,
-            this.RESEND_TIMEOUT,
-            message,
-            retry + 1
-          )
+        ? setTimeout(() => {
+          this.websocket.send(message)
+        }, this.RESEND_TIMEOUT)
         : this.websocket.send(JSON.stringify(message));
     } catch (error) {
       console.error(error);
