648541ba55719259b779afce48bfe22e4e98090b
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2023-09-07 09:21:53 +0100

    Restore webpack build

---
+++ webpack.config.cjs
@@ -0,0 +1,53 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve("./"),
+    filename: "dist/index.js",
+    library: "Movai",
+    libraryTarget: "umd"
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  target: "web",
+  devtool: "source-map",
+  // externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(js|mjs|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: [
+              "@babel/plugin-syntax-dynamic-import",
+              "@babel/plugin-syntax-import-meta",
+              "@babel/plugin-proposal-class-properties",
+              "@babel/plugin-proposal-json-strings",
+              [
+                "@babel/plugin-transform-runtime",
+                { useESModules: true, helpers: true }
+              ]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: "url-loader"
+        }
+      }
+    ]
+  }
+};
